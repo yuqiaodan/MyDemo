f39ccd8bd22905ec378388d86a498b53855d3e34
yuqiaodan <2031015269@qq.com>
2021-11-17 14:38:53 +0800

    test

--- app/src/main/java/com/yuqiaodan/mydemo/utils/ShellUtils.java
+++ app/src/main/java/com/yuqiaodan/mydemo/utils/ShellUtils.java
@@ -14,6 +14,34 @@ import java.io.InputStreamReader;
  * Description:
  **/
 public class ShellUtils {
+    /**
+     * 获取电池容量 mAh
+     *
+     * 源头文件:frameworks/base/core/res\res/xml/power_profile.xml
+     *
+     * Java 反射文件：frameworks\base\core\java\com\android\internal\os\PowerProfile.java
+     */
+    public static String getBatteryCapacity(Context context) {
+        Object mPowerProfile;
+        double batteryCapacity = 0;
+        final String POWER_PROFILE_CLASS = "com.android.internal.os.PowerProfile";
+
+        try {
+            mPowerProfile = Class.forName(POWER_PROFILE_CLASS)
+                    .getConstructor(Context.class)
+                    .newInstance(context);
+
+            batteryCapacity = (double) Class
+                    .forName(POWER_PROFILE_CLASS)
+                    .getMethod("getBatteryCapacity")
+                    .invoke(mPowerProfile);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return String.valueOf(batteryCapacity + " mAh");
+    }
 
     public static boolean enableAdb(Context context){
         int enable = Settings.Secure.getInt(context.getContentResolver(), Settings.Global.ADB_ENABLED, 0);
